a71432d5a88b2034cb01389abc52f73b11c4204a
DerHimbeerHugo <131753938+DerHimbeerHugo@users.noreply.github.com>
2026-08-26 14:52:14 +0200

    feat: add Eqiva constants

---
+++ custom_components/eqiva_keyble/const.py
@@ -0,0 +1,18 @@
+DOMAIN = "eqiva_keyble"
+PLATFORMS = ["lock", "binary_sensor"]
+
+CONF_ADDRESS = "address"
+CONF_USER_ID = "user_id"
+CONF_USER_KEY = "user_key"
+CONF_KEY_CARD = "key_card"
+CONF_NAME = "name"
+CONF_SETUP_METHOD = "setup_method"
+
+SETUP_KEY_CARD = "key_card"
+SETUP_CREDENTIALS = "credentials"
+
+DEFAULT_NAME = "Eqiva Türschloss"
+
+SERVICE_UUID = "58e06900-15d8-11e6-b737-0002a5d5c51b"
+SEND_CHARACTERISTIC_UUID = "3141dd40-15db-11e6-a24b-0002a5d5c51b"
+RECEIVE_CHARACTERISTIC_UUID = "359d4820-15db-11e6-82bd-0002a5d5c51b"
